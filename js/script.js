2bd55b0fe5155dbe23a513e642c5d30190a72380
priegels <88594441+priegels@users.noreply.github.com>
2021-09-06 19:40:39 +0200

    set up working to do list app using jQuery code

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+function newItem(){
+
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    let list = $('#list');
+    list.append(li);
+  }
+
+  li.on('dblclick', crossOut);
+    function crossOut() {
+      li.toggleClass('strike');
+    }
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+      li.addClass('delete');
+    }
+
+  $('#list').sortable();
+  }
